109f9c3ad883150b2baa395488f4854cb0db67e3
Erick <cardona.erick@hotmail.com>
2019-12-03 22:52:49 -0600

    added login verify, missing create

--- public/JS/user/userCreate.js
+++ public/JS/user/userCreate.js
@@ -89,47 +89,96 @@ function createUser(e){
     //to-do checar inputs invalidos
 
     // Check for invalid inputs
-    // let invalids = document.querySelectorAll("*:invalid").length;
-    // if(invalids > 0)
-    //     return;
+    let invalids = document.querySelectorAll("*:invalid");
+    let invalidsLen = invalids.length
+    if(invalidsLen >= 3){
+        console.log("Hay invalidos");
+        console.log(invalids);
+        return;
+    }
+    else{
+        console.log("No hay invalidos");
+        crearUsuarioPost(user);
+    }
+        
 
     // e.preventDefault();
 
         // Prepare and send request
-        let xhr = new XMLHttpRequest();
-        let url = serverInfo.hostname + serverInfo.port + serverInfo.userEndPoint;
-        console.log(user);
-        let data = JSON.stringify(user);
+        // let xhr = new XMLHttpRequest();
+        // let url = serverInfo.hostname + serverInfo.port + serverInfo.userEndPoint;
+        // console.log(user);
+        // let data = JSON.stringify(user);
 
-        console.log(data);
+        // console.log(data);
 
 
-        xhr.open("POST", url);
-        xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-        xhr.send(data);
+        // xhr.open("POST", url);
+        // xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+        // xhr.send(data);
 
-        xhr.onload = () => {
-            console.log(xhr.response);
-            let response = JSON.parse(xhr.response);
-            console.log(response);
+        // xhr.onload = () => {
+        //     console.log(xhr.response);
+        //     let response = JSON.parse(xhr.response);
+        //     console.log(response);
 
-            if(xhr.status == 201){
-                alert("Usuario creado correctamente, ya puedes iniciar sesión");
-                window.location.href = "../HTML/LandingPage.html"
-            } else {
-                console.log(response);
-                alert(response.error);
-            }
-        };
-        xhr.onerror = () => {
-            console.log(xhr.status);
-            console.log(xhr.statusText);
-            alert("Hubo un error al crear un usuario"); 
-        };
-
-    
+        //     if(xhr.status == 201){
+        //         alert("Usuario creado correctamente, ya puedes iniciar sesión");
+        //         // window.location.href = "../HTML/LandingPage.html"
+        //         modalLoginbtn.click()
+        //     } else {
+        //         console.log(response);
+        //         alert(response.error);
+        //     }
+        // };
+        // xhr.onerror = () => {
+        //     console.log(xhr.status);
+        //     console.log(xhr.statusText);
+        //     alert("Hubo un error al crear un usuario"); 
+        // };
     
 }
+
+function crearUsuarioPost(user){
+
+    // Prepare and send request
+    let xhr = new XMLHttpRequest();
+    let url = serverInfo.hostname + serverInfo.port + serverInfo.userEndPoint;
+    console.log(user);
+    let data = JSON.stringify(user);
+
+    console.log(data);
+
+
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    xhr.send(data);
+
+    xhr.onload = () => {
+        console.log(xhr.response);
+        let response = JSON.parse(xhr.response);
+        console.log(response);
+
+        if(xhr.status == 201){
+            alert("Usuario creado correctamente, ya puedes iniciar sesión");
+            // window.location.href = "../HTML/LandingPage.html"
+            modalLoginbtn.click()
+        } else {
+            console.log(response);
+            alert(response.error);
+        }
+    };
+    xhr.onerror = () => {
+        console.log(xhr.status);
+        console.log(xhr.statusText);
+        alert("Hubo un error al crear un usuario"); 
+    };
+
+}
+
+let modalLogin = document.getElementById("modelIdLogin")
+let modalLoginbtn = document.getElementById("loginBtn")
+
 
 function getLastId(){
 
