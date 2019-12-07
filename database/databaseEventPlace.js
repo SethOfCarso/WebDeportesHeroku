7f7b5ac47e3134f6482b78a34a28aefc29a21363
Elias <eGasparArellano@gmail.com>
2019-12-06 23:30:12 -0600

    Backend validations for Event Places

--- database/databaseEventPlace.js
+++ database/databaseEventPlace.js
@@ -9,9 +9,11 @@ let eventPlaceSchema = mongoose.Schema({
     nombreLugarEvento: {
         type: String,
         required: true,
+        unique: true
     },
     idDisciplinas: {
-        type: Array, 
+        type: Array,
+        default: undefined,
         required: true
     },
     fotoLugarEvento: {
@@ -31,10 +33,14 @@ let eventPlaceSchema = mongoose.Schema({
 let EventPlace = mongoose.model('eventplaces', eventPlaceSchema);
 
 async function createEventPlace(newEventPlace){
-    let eventPlace = EventPlace(newEventPlace);
-    // Create event
-    let docs = await eventPlace.save();
+    try{
+        let eventPlace = EventPlace(newEventPlace);
+        // Create event
+        let docs = await eventPlace.save();
     return docs;
+    } catch(e) {
+        return {error: e.errmsg};
+    }
 }
 
 async function getEventPlaces(){
@@ -44,8 +50,7 @@ async function getEventPlaces(){
         let docs = await EventPlace.find({}, {_id:0, __v: 0}, (err, docs) => {});
         return docs;
     } catch(e){
-        console.log(e)
-        console.log("Error de conexión");
+        return {error: e.errmsg};
     }
 }
 
@@ -57,30 +62,37 @@ async function getEventPlaceById(id){
 
         return docs;
     } catch(e){
-        console.log(e)
-        console.log("Error de conexión");
+        return {error: e.errmsg};
     }
 }
 
 async function updateEventPlace(id, eventPlace){
-    let docs = await EventPlace.findOneAndUpdate(
-        {id : id}, 
-        {$set:  {
-            "id" : eventPlace.id, 
-            "nombreLugarEvento" : eventPlace.nombreLugarEvento, 
-            "idDisciplinas" : eventPlace.idDisciplinas,
-            "fotoLugarEvento" : eventPlace.fotoLugarEvento, 
-            "direccion" : eventPlace.direccion,
-            "descripcion" : eventPlace.descripcion}
-        }, 
-        {returnNewDocument: true}
-    );
-    return docs;
+    try{
+        let docs = await EventPlace.findOneAndUpdate(
+            {id : id}, 
+            {$set:  {
+                "id" : eventPlace.id, 
+                "nombreLugarEvento" : eventPlace.nombreLugarEvento, 
+                "idDisciplinas" : eventPlace.idDisciplinas,
+                "fotoLugarEvento" : eventPlace.fotoLugarEvento, 
+                "direccion" : eventPlace.direccion,
+                "descripcion" : eventPlace.descripcion}
+            }, 
+            {returnNewDocument: true}
+        );
+        return docs;
+    } catch(e){
+        return {error: e.errmsg};
+    }
 }
 
 async function deleteEventPlace(id){
-    let doc = await EventPlace.findOneAndDelete({id : id});
-    return doc;
+    try{
+        let doc = await EventPlace.findOneAndDelete({id : id});
+        return doc;
+    } catch(e){
+        return {error: e.errmsg};
+    }
 }
 
 module.exports = {createEventPlace, getEventPlaces, getEventPlaceById, deleteEventPlace, updateEventPlace};
--- routers/routerEventPlaces.js
+++ routers/routerEventPlaces.js
@@ -9,9 +9,20 @@ router.route("/")
         res.send(eventPlaces);
     })
     .post(async (req, res) => {
-        let eventPlace = await db.createEventPlace(req.body);
-        res.status(201);
-        res.send(eventPlace);
+        let bodyEventPlaceInfo = checkBodyEventPlace(req.body);
+        if(bodyEventPlaceInfo.length > 7){
+            res.status(400);
+            res.send({error: bodyEventPlaceInfo});
+        } else {
+            let mongoResponse = await db.createEventPlace(req.body);
+            if(mongoResponse.error !== undefined){
+                res.status(400);
+                res.send({error: mongoResponse.error}); 
+            } else {
+                res.status(201);
+                res.send(mongoResponse);
+            }
+        }
     });
 
 router.route("/:id")
@@ -21,14 +32,38 @@ router.route("/:id")
         res.send(eventPlace);
     })
     .put(async (req, res) => {
-        let eventPlace = await db.updateEventPlace(req.params.id, req.body);
-        res.status(200);
-        res.send(eventPlace);
+        let bodyEventPlaceInfo = checkBodyEventPlace(req.body);
+        if(bodyEventPlaceInfo.length > 7){
+            res.status(400);
+            res.send({error: bodyEventPlaceInfo});
+        } else {
+            let mongoResponse = await db.updateEventPlace(req.params.id, req.body);
+            if(mongoResponse.error !== undefined){
+                res.status(400);
+                res.send({error: mongoResponse.error}); 
+            } else {
+                res.status(200);
+                res.send(mongoResponse);
+            }
+        }
     })
     .delete(async (req, res) => {
         let eventPlace = await db.deleteEventPlace(req.params.id);
         res.status(200);
         res.send(eventPlace);
     });
+
+let eventPlaceData = new Array("id", "fotoLugarEvento", "nombreLugarEvento", "direccion", "descripcion", "idDisciplinas");
+
+function checkBodyEventPlace(bodyRequest){
+    let info = "Falta: ";
+    // Search for a missing property
+    eventPlaceData.forEach((element) => {
+        if(bodyRequest[element] === undefined)
+            info += element + ", " 
+    });
+
+    return info;
+}
 
 module.exports = router;
