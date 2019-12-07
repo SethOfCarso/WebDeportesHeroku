bc8b83f5d96a8814ae56021a9edd0a61201553de
Elias <eGasparArellano@gmail.com>
2019-12-06 23:07:21 -0600

    Backend validations for Events

--- routers/routerEvents.js
+++ routers/routerEvents.js
@@ -9,9 +9,20 @@ router.route("/")
         res.send(events);
     })
     .post(async (req, res) => {
-        let event = await db.createEvent(req.body);
-        res.status(201);
-        res.send(event);
+        let bodyEventInfo = checkBodyEvent(req.body);
+        if(bodyEventInfo.length > 7){
+            res.status(400);
+            res.send({error: bodyEventInfo});
+        } else {
+            let mongoResponse = await db.createEvent(req.body);
+            if(mongoResponse.error !== undefined){
+                res.status(400);
+                res.send({error: mongoResponse.error}); 
+            } else {
+                res.status(201);
+                res.send(mongoResponse);
+            }
+        }
     });
 
 router.route("/:id")
@@ -21,14 +32,38 @@ router.route("/:id")
         res.send(event);
     })
     .put(async (req, res) => {
-        let event = await db.updateEvent(req.params.id, req.body);
-        res.status(200);
-        res.send(event);
+        let bodyEventInfo = checkBodyEvent(req.body);
+        if(bodyEventInfo.length > 7){
+            res.status(400);
+            res.send({error: bodyEventInfo});
+        } else {
+            let mongoResponse = await db.updateEvent(req.params.id, req.body);
+            if(mongoResponse.error !== undefined){
+                res.status(400);
+                res.send({error: mongoResponse.error}); 
+            } else {
+                res.status(200);
+                res.send(mongoResponse);
+            }
+        }
     })
     .delete(async (req, res) => {
         let event = await db.deleteEvent(req.params.id);
         res.status(200);
         res.send(event);
     });
+
+let eventData = new Array("id", "fotoEvento", "nombreEvento", "fechaInicio", "fechaFin", "encuentros");
+
+function checkBodyEvent(bodyRequest){
+    let info = "Falta: ";
+    // Search for a missing property
+    eventData.forEach((element) => {
+        if(bodyRequest[element] === undefined)
+            info += element + ", " 
+    });
+
+    return info;
+}
 
 module.exports = router;
